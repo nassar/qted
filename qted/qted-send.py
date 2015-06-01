cddf1425950220e19087f38420c5accd3f625c64
Nassib Nassar <nn@nassarsoftware.com>
2015-06-01 16:59:18 -0400

    Add arguments to send command

--- qted/qted-send.py
+++ qted/qted-send.py
@@ -7,6 +7,10 @@ def run(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='qted send')
+    parser.add_argument('panelid', nargs='?',
+        help='panel ID containing the list of recipients')
+    parser.add_argument('--all',
+        help="send to all panels that haven't been invited")
     args = parser.parse_args()
     run(args)
 
